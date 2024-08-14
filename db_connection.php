16332ebc1a35365019eb9cd23ccdcf29b5bd01e4
Thomas Cardoso <tcardo06@gmail.com>
2024-08-14 14:30:28 +0200

    fixed the config file

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost';
-$dbname = 'blogDB';
-$user = 'root';
-$pass = '';
+// $host = 'localhost';
+// $dbname = 'blogDB';
+// $user = 'root';
+// $pass = '';
 
-// $host = 'eu-cluster-west-01.k8s.cleardb.net';
-// $dbname = 'heroku_96c8bf64ca0644a';
-// $user = 'bc89c02bdec69e';
-// $pass = '2b493cfe';
+$host = 'eu-cluster-west-01.k8s.cleardb.net';
+$dbname = 'heroku_96c8bf64ca0644a';
+$user = 'bc89c02bdec69e';
+$pass = '2b493cfe';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 $conn->set_charset('utf8mb4');
