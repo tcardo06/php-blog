2a039615e3cac3e98bb2ccce4d3d13d8dda99061
Thomas Cardoso <tcardo06@gmail.com>
2024-08-14 14:57:42 +0200

    test connection with clearDB

--- db_connection.php
+++ db_connection.php
@@ -1,18 +1,17 @@
 <?php
-// $host = 'localhost';
-// $dbname = 'blogDB';
-// $user = 'root';
-// $pass = '';
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = 'eu-cluster-west-01.k8s.cleardb.net';
-$dbname = 'heroku_96c8bf64ca0644a';
-$user = 'bc89c02bdec69e';
-$pass = '2b493cfe';
+$host = $url["host"];
+$dbname = substr($url["path"], 1);
+$user = $url["user"];
+$pass = $url["pass"];
 
 $conn = new mysqli($host, $user, $pass, $dbname);
+
 $conn->set_charset('utf8mb4');
 
 if ($conn->connect_error) {
     throw new Exception("Connection failed: " . $conn->connect_error);
 }
+
 ?>
