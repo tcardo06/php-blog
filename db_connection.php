2c4db767eb812b3853676e16f3bee419f62e1a17
Thomas Cardoso <tcardo06@gmail.com>
2024-08-23 18:36:22 +0200

    changed connection error method

--- db_connection.php
+++ db_connection.php
@@ -19,7 +19,7 @@ $conn = new mysqli($host, $user, $pass, $dbname);
 $conn->set_charset('utf8mb4');
 
 if ($conn->connect_error) {
-    throw new Exception("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 ?>
