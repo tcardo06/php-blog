f3fb56bc591be2c98e474f20d80ca77b73066e62
Thomas Cardoso <tcardo06@gmail.com>
2024-08-23 10:19:50 +0200

    added connection to InfiniteFree database

--- db_connection.php
+++ db_connection.php
@@ -1,17 +1,22 @@
-<?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+<?php// $host = 'localhost';
+// $dbname = 'blogDB';
+// $user = 'root';
+// $pass = '';
 
-$host = $url["host"];
-$dbname = substr($url["path"], 1);
-$user = $url["user"];
-$pass = $url["pass"];
+// $host = 'eu-cluster-west-01.k8s.cleardb.net';
+// $dbname = 'heroku_96c8bf64ca0644a';
+// $user = 'bc89c02bdec69e';
+// $pass = '2b493cfe';
+
+$host = 'sql110.infinityfree.com';
+$dbname = 'if0_37156188_blogdb';
+$user = 'if0_37156188';
+$pass = 'Vn1cit1GzZJx7';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
-
 $conn->set_charset('utf8mb4');
 
 if ($conn->connect_error) {
-    throw new Exception("Connection failed: " . $conn->connect_error);
+    throw new Exception("Connection failed: " . $conn->connect_error);
 }
-
 ?>
