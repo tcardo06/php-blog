d3a394d46e3dcbdf302076eb7fdc71bb2b722bf8
Thomas Cardoso <tcardo06@gmail.com>
2024-08-23 18:34:31 +0200

    changed connection loop for test

--- db_connection.php
+++ db_connection.php
@@ -19,9 +19,7 @@ $conn = new mysqli($host, $user, $pass, $dbname);
 $conn->set_charset('utf8mb4');
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connection successful!";
+    throw new Exception("Connection failed: " . $conn->connect_error);
 }
 
 ?>
