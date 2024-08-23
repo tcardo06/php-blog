570f45b8d6484956a9ac722e94026498b11d22e9
Thomas Cardoso <tcardo06@gmail.com>
2024-08-23 18:59:40 +0200

    cleaDB con test

--- db_connection.php
+++ db_connection.php
@@ -19,7 +19,10 @@ $conn = new mysqli($host, $user, $pass, $dbname);
 $conn->set_charset('utf8mb4');
 
 if ($conn->connect_error) {
+    throw new Exception("Connection failed: " . $conn->connect_error);
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connection successful!";
 }
 
 ?>
