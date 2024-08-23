71d3b639273beb66064ce217cc9116f903017df4
Thomas Cardoso <tcardo06@gmail.com>
2024-08-23 18:27:57 +0200

    connection test to clearDB

--- db_connection.php
+++ db_connection.php
@@ -5,15 +5,15 @@
 // $user = 'root';
 // $pass = '';
 
-// $host = 'eu-cluster-west-01.k8s.cleardb.net';
-// $dbname = 'heroku_96c8bf64ca0644a';
-// $user = 'bc89c02bdec69e';
-// $pass = '2b493cfe';
+$host = 'eu-cluster-west-01.k8s.cleardb.net';
+$dbname = 'heroku_96c8bf64ca0644a';
+$user = 'bc89c02bdec69e';
+$pass = '2b493cfe';
 
-$host = 'sql110.infinityfree.com';
-$dbname = 'if0_37156188_blogdb';
-$user = 'if0_37156188';
-$pass = 'Vn1cit1GzZJx7';
+// $host = 'sql110.infinityfree.com';
+// $dbname = 'if0_37156188_blogdb';
+// $user = 'if0_37156188';
+// $pass = 'Vn1cit1GzZJx7';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 $conn->set_charset('utf8mb4');
