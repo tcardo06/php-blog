ed86844cbc7066ab14ae563d417c5a2b5ef9811a
Thomas Cardoso <tcardo06@gmail.com>
2024-09-04 12:18:17 +0200

    fix security issues + removed phone from contact mail

--- db_connection.php
+++ db_connection.php
@@ -20,7 +20,6 @@ $conn->set_charset('utf8mb4');
 
 if ($conn->connect_error) {
     throw new Exception("Connection failed: " . $conn->connect_error);
-    die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 ?>
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,7 +18,6 @@ if(empty($_POST['name']) ||
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = isset($_POST['phone']) ? strip_tags(htmlspecialchars($_POST['phone'])) : '';
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create a new PHPMailer instance
@@ -27,10 +26,10 @@ $mail = new PHPMailer(true);
 try {
     // Server settings
     $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';              
+    $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = getenv('GMAIL_USERNAME');  
-    $mail->Password = getenv('GMAIL_PASSWORD');  
+    $mail->Username = getenv('GMAIL_USERNAME');
+    $mail->Password = getenv('GMAIL_PASSWORD');
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
@@ -39,23 +38,22 @@ try {
     $mail->addAddress(getenv('GMAIL_USERNAME'), 'Tom');
 
     // Content
-    $mail->isHTML(true);                                  
+    $mail->isHTML(true);
     $mail->Subject = "Website Contact Form:  $name";
     $mail->Body    = "You have received a new message from your website contact form.<br><br>" .
                      "Here are the details:<br><br>" .
                      "Name: $name<br><br>Email: $email_address<br><br>" .
-                     "Phone: $phone<br><br>Message:<br>$message";
     $mail->AltBody = "You have received a new message from your website contact form.\n\n" .
                      "Here are the details:\n\n" .
                      "Name: $name\n\nEmail: $email_address\n\n" .
-                     "Phone: $phone\n\nMessage:\n$message";
 
     // Send email
     $mail->send();
 
     // Redirect to success.php after sending the email
     header("Location: success.php");
-    exit();
+    return;
+
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     return false;
